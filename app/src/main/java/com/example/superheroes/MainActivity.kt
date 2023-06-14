8a8484ba058edbc85bae4acd52d8486ca058bd42
thedarkarchitect <romanreynolds240@gmail.com>
2023-06-14 13:08:14 +0300

    second commit

--- app/src/main/java/com/example/superheroes/MainActivity.kt
+++ app/src/main/java/com/example/superheroes/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SuperheroApp()
                 }
             }
         }
@@ -30,17 +30,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun SuperheroApp(modifier: Modifier = Modifier) {
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SuperheroesTheme {
-        Greeting("Android")
+        SuperheroApp()
     }
 }
--- app/src/main/java/com/example/superheroes/ui/theme/Type.kt
+++ app/src/main/java/com/example/superheroes/ui/theme/Type.kt
@@ -2,9 +2,16 @@ package com.example.superheroes.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.superheroes.R
+
+val Cabin = FontFamily(
+    Font(R.font.Cabin_Regular),
+    Font(R.font.Cabin_Bold, FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
